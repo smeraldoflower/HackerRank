c0247213c2df83d8f48dc025bd2f992f3c260733
Nusaiba <57018533+smeraldoflower@users.noreply.github.com>
2020-10-17 20:23:09 -0500

    Added HackerRank JAVA files

---
+++ Practice Problems/01_Intro_welcome-to-java.java
@@ -0,0 +1,13 @@
+// PROBLEM: https://www.hackerrank.com/challenges/welcome-to-java/problem
+
+public class Solution
+{
+
+    public static void main(String[] args)
+    {
+     /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
---
+++ Practice Problems/02_Intro_java-stdin-and-stdout-1.java
@@ -0,0 +1,24 @@
+// PROBLEM : https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem
+
+import java.util.*;
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+              
+        scan.close();
+    }
+}
